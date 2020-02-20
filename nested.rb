94d2261de3728dc8bbc36d8a2e9c8c75079c2542
William Kennedy <alexkenned@gmail.com>
2020-02-20 21:16:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -53,10 +53,10 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => "C"
+          :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
